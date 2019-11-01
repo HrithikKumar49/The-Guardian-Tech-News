9155b80cc88e4cdf4115c47f62415b5721354efa
HrithikKumar49 <57259040+HrithikKumar49@users.noreply.github.com>
2019-11-01 18:41:14 +0530

    Add files via upload
    Added the exisiting local project

---
+++ README.md
@@ -0,0 +1,21 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This App Displays Top 10 Headlines from [The Gaurdian Newspaper Website](https://www.theguardian.com/) using its [API](https://open-platform.theguardian.com/)
+
+
+## Install dependencies
+
+`yarn install`
+
+## Run the App
+
+`yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+
+
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import logo from './logo.png'
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      headlines: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const url = "https://content.guardianapis.com/technology";
+      const api_key = "2bcc5ab8-2963-4b20-8bd4-ce30f67268e8";
+      const configs = {
+        params: {
+          "api-key": api_key
+        }
+      };
+      const res = await axios.get(url, configs);
+      console.log(res.data);
+      let result = [];
+      res.data.response.results.forEach(element => {
+        result.push(element);
+      });
+
+      this.setState({
+        headlines: result
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="The Gaurdian logo"></img>
+          <h1>
+            Tech News
+          </h1>
+          
+          <h3>
+            Top Headlines
+          </h3>
+          <ul style={{listStyleType:'none'}}>
+            {this.state.headlines.map((element) => {
+              return <li style={{margin:'10px'}}><a href={element.webUrl} style={{color:'white'}}>{element.webTitle}</a></li>
+            })}
+          </ul>
+        </header>
+      </div>);
+  }
+}
+
+export default App;
