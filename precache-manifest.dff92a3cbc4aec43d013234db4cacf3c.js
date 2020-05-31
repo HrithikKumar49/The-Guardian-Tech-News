1a0ea4d7cb1c5e4ab500d3e1136237c0f6748fb8
KKghub <KKghub@users.noreply.github.com>
2020-05-31 11:50:59 +0000

    deploy: 103765568a8f58af94449b6574fcbb314ac9915e

---
+++ precache-manifest.dff92a3cbc4aec43d013234db4cacf3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3e20d4233d19460bac5bf23bbf9c64",
+    "url": "/The-Guardian-Tech-News/index.html"
+  },
+  {
+    "revision": "9687680bd7b20a40a0e2",
+    "url": "/The-Guardian-Tech-News/static/css/main.fdf09033.chunk.css"
+  },
+  {
+    "revision": "6da2e9ace489035d940c",
+    "url": "/The-Guardian-Tech-News/static/js/2.b4bbf9bc.chunk.js"
+  },
+  {
+    "revision": "9687680bd7b20a40a0e2",
+    "url": "/The-Guardian-Tech-News/static/js/main.05f162da.chunk.js"
+  },
+  {
+    "revision": "378c4ff94aae24f102b6",
+    "url": "/The-Guardian-Tech-News/static/js/runtime-main.48daeda0.js"
+  },
+  {
+    "revision": "93b8d6e970f76663e4fea98033ea5608",
+    "url": "/The-Guardian-Tech-News/static/media/logo.93b8d6e9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/The-Guardian-Tech-News/precache-manifest.2932912a79d425fd6b4626e5805636b0.js"
+  "/The-Guardian-Tech-News/precache-manifest.dff92a3cbc4aec43d013234db4cacf3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/The-Guardian-Tech-News/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
