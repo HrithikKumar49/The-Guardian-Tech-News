d82bc4e20e4eba8d27fbba8a05edfe4a9dc1b9a4
KKghub <kartikey.zen@gmail.com>
2020-05-31 16:46:47 +0530

    Updates

---
+++ precache-manifest.facbe7235bfe70126ceb9e044f670835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885a1fb1b2b007e6bd2377628bb20598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bddcec2eccc848652a97",
+    "url": "/static/css/main.f4db3eaf.chunk.css"
+  },
+  {
+    "revision": "6da2e9ace489035d940c",
+    "url": "/static/js/2.b4bbf9bc.chunk.js"
+  },
+  {
+    "revision": "bddcec2eccc848652a97",
+    "url": "/static/js/main.5081b1b8.chunk.js"
+  },
+  {
+    "revision": "f885ebc6c45987211826",
+    "url": "/static/js/runtime-main.c18a0d0f.js"
+  },
+  {
+    "revision": "93b8d6e970f76663e4fea98033ea5608",
+    "url": "/static/media/logo.93b8d6e9.png"
+  }
+]);
